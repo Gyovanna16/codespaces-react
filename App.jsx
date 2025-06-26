2f86df4585cf7cbe41454695fda3ca58863ad9d4
Gyovanna16 <jaidegyovanna@gmail.com>
2025-06-26 17:07:03 +0000

    Desafio4

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import "./styles/theme.css";
+import "./styles/global.css";
+import { MyTextList } from "./components/MyTextList";
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
+import { Footer } from "./components/Footer";
+import { LuckyNumber } from "./components/ETAPA2/LuckyNumber";
+
+export default function App() {
+  return (
+    <>
+        <LuckyNumber />
+    </>
+  );
+}
